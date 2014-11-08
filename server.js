256d4fe198fed965cd75ff201094d3b42e8d2e9c
Chareesa <chareesagraham@gmail.com>
2014-11-08 13:32:36 -0800

    changed res.json in server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ mongoose.connect(url);
 require('./routes/notes_routes')(app);
 
 app.get('/', function (req, res) {
-  res.json('{"msg": "well hello there!"}')
-})
+  res.json({"msg": "well hello there!"});
+});
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
