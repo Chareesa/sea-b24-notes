ad9c118e975e5a6316f1646298ab1ec30dc710fd
Chareesa <chareesagraham@gmail.com>
2014-11-08 13:27:10 -0800

    added a GET

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ mongoose.connect(url);
 
 require('./routes/notes_routes')(app);
 
+app.get('/', function (res, req) {
+  res.json('{"msg": "well hello there!"}')
+})
+
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
   console.log('server running on port: %d', app.get('port'));
